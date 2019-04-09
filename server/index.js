8dcedb3455766710ff8a1d2e02cab16e5779d11a
torinberger <torinberger@torinberger.com>
2019-04-09 18:46:54 +1000

    koa/router setups

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 
+const koa = require('koa');
+const router = require('koa-joi-router');
+
+const api = router();
+
+api.url('/api')
+
+const app = new koa();
+app.use(api.middleware());
+app.listen(3000);
