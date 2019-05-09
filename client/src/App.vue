2ddeda1393ae5a02752148a64472d54c89b3c71f
torinberger <torinberger@torinberger.com>
2019-05-09 04:56:37 +0000

    upgraded to quasar v1.0.beta

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,53 +1,14 @@
 <template>
-  <div id="app">
-
-    <q-layout view="hHr LpR lFf">
-      <!-- Header -->
-      <q-layout-header>
-        <!-- First row of header is a QToolbar -->
-        <q-toolbar color="dark">
-
-          <q-toolbar-title>
-            Layout Header
-          </q-toolbar-title>
-
-          <!-- showRight is a model attached to right side drawer below -->
-          <q-btn
-            flat round dense
-            @click="showRight = !showRight"
-            icon="menu"
-          />
-        </q-toolbar>
-
-      </q-layout-header>
-
-      <!-- Right Side Panel -->
-      <q-layout-drawer side="right" v-model="showRight" :overlay="true">
-        Right Side of Layout
-      </q-layout-drawer>
-
-      <!-- sub-routes get injected here: -->
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-    </q-layout>
-
+  <div id="q-app">
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      showRight: false
-    }
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-.q-loading-bar {
-  display: none;
-}
 </style>
