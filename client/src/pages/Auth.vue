a2aaad80f8f830bb50b49b125d76764851973a08
torinberger <torinberger@torinberger.com>
2019-05-21 03:12:53 +0000

    better looking auth login

--- client/src/pages/Auth.vue
+++ client/src/pages/Auth.vue
@@ -1,13 +1,40 @@
 <template>
   <div id="auth">
-    <div v-if="mode == 'login'" class="auth-tab">
+    <!-- <div v-if="mode == 'login'" class="auth-tab">
       <h3>Login</h3>
       <p>Or <a @click="switchMode">Register</a></p>
     </div>
     <div v-if="mode == 'register'" class="auth-tab">
       <h3>Register</h3>
       <p>Or <a @click="switchMode">Login</a></p>
-    </div>
+    </div> -->
+    <q-card class="auth-tab">
+      <q-tabs
+        v-model="mode"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+      >
+        <q-tab name="login" label="Login"/>
+        <q-tab name="register" label="Register" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="mode" animated>
+        <q-tab-panel name="login">
+          <div class="text-h6">Login</div>
+          Login pls
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <div class="text-h6">Register</div>
+          <q-input v-model="name" label="Name" :rules="[ val => val && val.length > 0 || 'Please enter a name']"/>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </div>
 </template>
 
@@ -16,7 +43,8 @@ export default {
   name: 'Auth',
   data () {
     return {
-      mode: this.$route.params.mode
+      mode: this.$route.params.mode,
+      name: null
     }
   },
   created () {
@@ -39,6 +67,15 @@ export default {
         `/#/auth/${this.mode}`
       )
     }
+  },
+  watch: {
+    mode (val) {
+      history.pushState(
+        { urlPath: `/#/auth/${val}` },
+        '',
+        `/#/auth/${val}`
+      )
+    }
   }
 }
 </script>
@@ -56,7 +93,7 @@ h3 {
 
 .auth-tab {
   margin: auto;
-  height: 80vh;
+  height: 60vh;
   width: 20vw;
 }
 </style>
