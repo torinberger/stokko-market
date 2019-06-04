ee595540f2f1030c0e8c2cd796165d6fadb79a8d
torinberger <torinberger@torinberger.com>
2019-06-04 16:44:30 +1000

    much better revised user controller

--- server/database/controllers/user.js
+++ server/database/controllers/user.js
@@ -1,54 +1,54 @@
 const User = require('../models/user')
 
+// Structure
+//  class
+//    .function(params)
+//    .then(callback)
+//    .err(callback)
+
+// Methods
+//  GET -> getUsers ()
+//  GET -> getUserByID (id)
+//  GET -> getUser (target)
+//  ADD -> addUser (target)
+//  PUT -> updateUser (id, changeCallback)
+//    CALLBACK -> changeCallback (user, save) // within callback, user is changed in whatever way, then returned with save (user)
+
 module.exports = function () {
   return {
-    async getUsers (callback) {
-      User.find({}, function (err, data) {
-        if (err) {
-          callback(err)
-          throw err
-        } else {
-          callback(data)
-        }
-      })
+    async getUsers () {
+      return User.find({}).exec()
     },
-    async getUser (id, callback) {
-      User.findById(id, function (err, data) {
-        if (err) {
-          callback(err)
-          throw err
-        } else {
-          callback(data)
-        }
-      })
+    async getUserByID (id) {
+      return User.findByID(id).exec()
     },
-    async validateUser (target, callback) {
-      User.findOne(target, function (err, data) {
-        if (err) {
-          callback(err)
-        } else {
-          callback(data)
-        }
-      })
+    async getUser (target) {
+      return User.find(target).exec()
     },
-    async addUser (body, callback) {
-      new User(body).save(function (err, user) {
-        if (err) {
-          callback(err)
-          throw err
-        } else {
-          callback(user)
-        }
-      })
+    async addUser (target) {
+      return new User(target).save()
     },
-    async updateUser (id, changes, callback) {
-      User.updateOne({ _id: id }, changes, function (err) {
-        if (err) {
-          callback(err)
-          throw err
-        } else {
-          callback()
-        }
+    async updateUser (id, changeCallback) {
+      return new Promise(function (resolve, reject) {
+        User.findById(id, function (err, data) {
+          if (err) {
+            reject(err)
+          } else {
+            if (data) {
+              changeCallback(data, function (newUser) {
+                new User(newUser).save(function (err, updatedUser) {
+                  if (err) {
+                    reject(err)
+                  } else {
+                    resolve(updatedUser)
+                  }
+                })
+              })
+            } else {
+              reject(new Error('User Not Found'))
+            }
+          }
+        })
       })
     }
   }
